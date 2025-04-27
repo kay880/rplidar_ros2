2d8729ffe8b12100c25ba677599afb5af6c95623
kay880 <643805493@qq.com>
2025-04-27 13:25:56 +0800

    Add files via upload
    2

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -4,7 +4,8 @@ echo "remap the device serial port(ttyUSBX) to rplidar"
 echo "rplidar usb connection as /dev/rplidar, check it using the command : ls -l /dev | grep ttyUSB"
 echo "start copy rplidar.rules to /etc/udev/rules.d/"
 source /usr/share/colcon_cd/function/colcon_cd.sh
-sudo cp rplidar.rules  /etc/udev/rules.d
+colcon_cd rplidar_ros
+sudo cp scripts/rplidar.rules  /etc/udev/rules.d
 echo -e "\nRestarting udev\n"
 sudo service udev reload
 sudo service udev restart
